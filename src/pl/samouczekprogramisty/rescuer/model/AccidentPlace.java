aedf3f215107bf76a6ce87b865c63f102e3925a8
Maciek <maciej.harazinski@gmail.com>
2016-04-03 23:02:57 +0200

    RescueTeamMember getters

--- src/pl/samouczekprogramisty/rescuer/model/AccidentPlace.java
+++ src/pl/samouczekprogramisty/rescuer/model/AccidentPlace.java
@@ -6,10 +6,10 @@ import java.time.LocalDate;
  * Created by Maciek on 30.03.2016.
  */
 public class AccidentPlace {
-    Position position;
-    String description;
-    LocalDate date;
-    AccidentStatus status;
+    private Position position;
+    private String description;
+    private LocalDate date;
+    private AccidentStatus status;
 
     public enum AccidentStatus {
         ACTIVE, ASSIGEND, CLOSED
--- src/pl/samouczekprogramisty/rescuer/model/RescueTeamMember.java
+++ src/pl/samouczekprogramisty/rescuer/model/RescueTeamMember.java
@@ -7,11 +7,45 @@ import java.util.ArrayList;
  * Created by Maciek on 23.03.2016.
  */
 public class RescueTeamMember {
-    Position position;
-    LocalTime lastUpdated;
-    String name;
-    String surname;
-    String phoneNumber:;
-    List<Activity> activityLog = new ArrayList<Activity>();
+    private Position position;
+    private LocalTime lastUpdated;
+    private String name;
+    private String surname;
+    private String phoneNumber;
+    private List<Activity> activityLog = new ArrayList<Activity>();
+
+    public RescueTeamMember(Position position, LocalTime lastUpdated, String name, String surname, String phoneNumber) {
+        this.position = position;
+        this.lastUpdated = lastUpdated;
+        this.name = name;
+        this.surname = surname;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public Position getPosition() {
+        return position;
+    }
+
+
+    public LocalTime getLastUpdated() {
+        return lastUpdated;
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public List<Activity> getActivityLog() {
+        return activityLog;
+    }
 
 }
